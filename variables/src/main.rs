3a4295fe6ceb7670d07411f7faf1e4793362e204
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-04 19:33:01 -0700

    Checking Variables types and properties

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,21 @@ fn main() {
     // Changing the value of mutable variable
     x = 6;
     println!("The value of x is: {x}");
+
+    // Creating const string variable
+    const _CONSTANT_STRING: &str = "CONSTANT VARIABLE";
+
+    // Creating const i32 variable
+    const _CONSTANT_NUMBER: i32 = 123134514;
+
+    // Shadowing
+    let x = 5;
+    println!("The value of x after shadowing is {x}"); 
+
+    {
+        let x = x * 3;
+        println!("The value of x in inner scope is {x}");
+    }
+
+    println!("The value of x after completing execution of inner scope is {x}");
 }
