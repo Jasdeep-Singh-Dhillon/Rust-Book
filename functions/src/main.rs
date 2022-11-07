dd8e74eb46de0793ea03d34d4df38db2c9f6a36e
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-06 22:37:33 -0800

    Statement and Expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,19 @@ fn main() {
 
     another_function();
     parameterized_function(5);
+    print_labeled_measurement(5, "meters");
+    // Char does not get converted to string automatically
+    // print_labeled_measurement(123, 'm');
+
+    // Statement - Does not return any value
+    let _x = 6;
+
+    // Expression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is {y}");
 }
 
 // Snake case for function names; Declaration can be above or below as long as it is in the scope
@@ -12,4 +25,8 @@ fn another_function() {
 
 fn parameterized_function(x: i32) {
     println!("The value passed is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: &str) {
+    println!("The measurement is {value} {unit_label}");
 }
