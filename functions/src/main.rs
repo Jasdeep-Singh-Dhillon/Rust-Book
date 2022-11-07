f346151aa314f74096c91aa92abc5d2891684e5f
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-06 22:18:16 -0800

    Creating a parameterized function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,9 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    parameterized_function(5);
 }
 
 // Snake case for function names; Declaration can be above or below as long as it is in the scope
 fn another_function() {
     println!("Another function");
+}
+
+fn parameterized_function(x: i32) {
+    println!("The value passed is {x}");
 }
