2f963e9dae7d4529fc47d356ccb1c41b26632c61
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-06 22:17:54 -0800

    Creating a function

---
+++ functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+}
+
+// Snake case for function names; Declaration can be above or below as long as it is in the scope
+fn another_function() {
+    println!("Another function");
+}
