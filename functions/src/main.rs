eca69d471b97859694718268737ecc2938915abc
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-06 22:43:27 -0800

    Returning using expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -13,9 +13,14 @@ fn main() {
     // Expression
     let y = {
         let x = 3;
-        x + 1
+        x + 1 // Expression
+        // x + 1; // Statement
     };
     println!("The value of y is {y}");
+
+    println!("Printing the return value of get_five() function: {}", get_five());
+
+    println!("Printing plus one value with parameter 6: {}", plus_one(6));
 }
 
 // Snake case for function names; Declaration can be above or below as long as it is in the scope
@@ -29,4 +34,12 @@ fn parameterized_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: &str) {
     println!("The measurement is {value} {unit_label}");
+}
+
+fn get_five() -> i32 {
+    5 // return value
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
