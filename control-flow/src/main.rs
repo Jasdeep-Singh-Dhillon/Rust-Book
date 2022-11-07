6c23f4cb1c4880375c3b00336eac52f69e4444f0
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2022-11-06 23:05:13 -0800

    Using else if to check if the entered number is divisible by 2, 3 or 4

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -28,9 +28,14 @@ fn main() {
         }             
     } 
 
-    if number < 5 {
-        println!("condition was true");
+    // If statement requires a bool value or boolean expression
+    if number % 4 == 0 {
+        println!("Number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("Number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("Number is divisible by 2");
     } else {
-        println!("condition was false");
+        println!("Number is not divisible by 2, 3 or 4");
     }
 }
